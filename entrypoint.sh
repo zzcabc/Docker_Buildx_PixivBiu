251606b83d829bd3a9d5f8bff1b8b5d951b0f3ed
zhaocheng <zhaocheng990105@gmail.com>
2025-11-11 10:57:46 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 BASE_URI="0.0.0.0"
-PORT=4001
+PORT=${PORT:-4001}
 DOWNLOAD_PATH=/Pixiv/downloads
 USER_PATH=/Pixiv/usr
 CONFIG_FILE=/Pixiv/config.yml
@@ -41,106 +41,112 @@ get_env() {
 env
 SYS_DEBUG=$(get_env "sys.debug")
 # SYS_HOST=$(get_env "sys.host")
-SYS_API_ROUTE=$(get_env "sys.apiRoute")
+SYS_APIROUTE=$(get_env "sys.apiRoute")
 SYS_PROXY=$(get_env "sys.proxy")
 SYS_LANGUAGE=$(get_env "sys.language")
 SYS_THEME=$(get_env "sys.theme")
-BIU_SEARCH_MAX_THREADS=$(get_env "biu.search.maxThreads")
-BIU_SEARCH_LOAD_CACHE_FIRST=$(get_env "biu.search.loadCacheFirst")
-BIU_SEARCH_MAX_CACHE_SIZE_MIB=$(get_env "biu.search.maxCacheSizeMiB")
+BIU_SEARCH_MAXTHREADS=$(get_env "biu.search.maxThreads")
+BIU_SEARCH_LOADCACHEFIRST=$(get_env "biu.search.loadCacheFirst")
+BIU_SEARCH_MAXCACHESIZEMIB=$(get_env "biu.search.maxCacheSizeMiB")
 BIU_DOWNLOAD_MODE=$(get_env "biu.download.mode")
-BIU_DOWNLOAD_ARIA2_HOST=$(get_env "biu.download.aria2Host")
-BIU_DOWNLOAD_ARIA2_SECRET=$(get_env "biu.download.aria2Secret")
-BIU_DOWNLOAD_DETER_PATHS=$(get_env "biu.download.deterPaths")
-BIU_DOWNLOAD_MAX_DOWNLOADING=$(get_env "biu.download.maxDownloading")
-BIU_DOWNLOAD_SAVE_URI=$(get_env "biu.download.saveURI")
-BIU_DOWNLOAD_SAVE_FILE_NAME=$(get_env "biu.download.saveFileName")
-BIU_DOWNLOAD_AUTO_ARCHIVE=$(get_env "biu.download.autoArchive")
-BIU_DOWNLOAD_WHATS_UGOIRA=$(get_env "biu.download.whatsUgoira")
-BIU_DOWNLOAD_IMAGE_HOST=$(get_env "biu.download.imageHost")
-SECRET_KEY_API_SAUCENAO=$(get_env "secret.key.apiSauceNAO")
+BIU_DOWNLOAD_ARIA2HOST=$(get_env "biu.download.aria2Host")
+BIU_DOWNLOAD_ARIA2SECRET=$(get_env "biu.download.aria2Secret")
+BIU_DOWNLOAD_DETERPATHS=$(get_env "biu.download.deterPaths")
+BIU_DOWNLOAD_MAXDOWNLOADING=$(get_env "biu.download.maxDownloading")
+BIU_DOWNLOAD_SAVEURI=$(get_env "biu.download.saveURI")
+BIU_DOWNLOAD_SAVEFILENAME=$(get_env "biu.download.saveFileName")
+BIU_DOWNLOAD_AUTOARCHIVE=$(get_env "biu.download.autoArchive")
+BIU_DOWNLOAD_WHATSUGOIRA=$(get_env "biu.download.whatsUgoira")
+BIU_DOWNLOAD_IMAGEHOST=$(get_env "biu.download.imageHost")
+SECRET_KEY_APISAUCENAO=$(get_env "secret.key.apiSauceNAO")
+
+# 对于每个参数，只有当对应的环境变量有值时才添加
+if [ -n "$PORT" ]; then
+    SYS_HOST="$BASE_URI:$PORT"
+fi
+if [ -n "$SYS_APIROUTE" ]; then
+    SYS_APIROUTE="$SYS_APIROUTE"
+fi
+if [ -n "$HTTPS_PROXY" ]; then
+    SYS_PROXY="$HTTPS_PROXY"
+fi
+if [ -n "$HTTP_PROXY" ]; then
+    SYS_PROXY="$HTTP_PROXY"
+fi
+if [ -n "$SYS_PROXY" ]; then
+    SYS_PROXY="$SYS_PROXY"
+fi
+if [ -n "$SYS_LANGUAGE" ]; then
+    SYS_LANGUAGE="$SYS_LANGUAGE"
+fi
+if [ -n "$SYS_THEME" ]; then
+    SYS_THEME="$SYS_THEME"
+fi
+if [ -n "$BIU_SEARCH_MAXTHREADS" ]; then
+    BIU_SEARCH_MAXTHREADS="$BIU_SEARCH_MAXTHREADS"
+fi
+if [ -n "$BIU_SEARCH_LOADCACHEFIRST" ]; then
+    BIU_SEARCH_LOADCACHEFIRST="$BIU_SEARCH_LOADCACHEFIRST"
+fi
+if [ -n "$BIU_SEARCH_MAXCACHESIZEMIB" ]; then
+    BIU_SEARCH_MAXCACHESIZEMIB="$BIU_SEARCH_MAXCACHESIZEMIB"
+fi
+if [ -n "$BIU_DOWNLOAD_MODE" ]; then
+    BIU_DOWNLOAD_MODE="$BIU_DOWNLOAD_MODE"
+fi
+if [ -n "$BIU_DOWNLOAD_ARIA2HOST" ]; then
+    BIU_DOWNLOAD_ARIA2HOST="$BIU_DOWNLOAD_ARIA2HOST"
+fi
+if [ -n "$BIU_DOWNLOAD_ARIA2SECRET" ]; then
+    BIU_DOWNLOAD_ARIA2SECRET="$BIU_DOWNLOAD_ARIA2SECRET"
+fi
+if [ -n "$BIU_DOWNLOAD_DETERPATHS" ]; then
+    BIU_DOWNLOAD_DETERPATHS="$BIU_DOWNLOAD_DETERPATHS"
+fi
+if [ -n "$BIU_DOWNLOAD_MAXDOWNLOADING" ]; then
+    BIU_DOWNLOAD_MAXDOWNLOADING="$BIU_DOWNLOAD_MAXDOWNLOADING"
+fi
+if [ -n "$BIU_DOWNLOAD_SAVEURI" ]; then
+    BIU_DOWNLOAD_SAVEURI="$BIU_DOWNLOAD_SAVEURI"
+fi
+if [ -n "$BIU_DOWNLOAD_SAVEFILENAME" ]; then
+    BIU_DOWNLOAD_SAVEFILENAME="$BIU_DOWNLOAD_SAVEFILENAME"
+fi
+if [ -n "$BIU_DOWNLOAD_AUTOARCHIVE" ]; then
+    BIU_DOWNLOAD_AUTOARCHIVE="$BIU_DOWNLOAD_AUTOARCHIVE"
+fi
+if [ -n "$BIU_DOWNLOAD_WHATSUGOIRA" ]; then
+    BIU_DOWNLOAD_WHATSUGOIRA="$BIU_DOWNLOAD_WHATSUGOIRA"
+fi
+if [ -n "$BIU_DOWNLOAD_IMAGEHOST" ]; then
+    BIU_DOWNLOAD_IMAGEHOST="$BIU_DOWNLOAD_IMAGEHOST"
+fi
+if [ -n "$SECRET_KEY_APISAUCENAO" ]; then
+    SECRET_KEY_APISAUCENAO="$SECRET_KEY_APISAUCENAO"
+fi
+
 echo "Ready to start PixivBiu with environment variable configurations."
 echo "sys.debug=$SYS_DEBUG"
-echo "sys.apiRoute=$SYS_API_ROUTE"
+echo "sys.host=$SYS_HOST"
+echo "sys.apiRoute=$SYS_APIROUTE"
 echo "sys.proxy=$SYS_PROXY"
 echo "sys.language=$SYS_LANGUAGE"
 echo "sys.theme=$SYS_THEME"
-echo "biu.search.maxThreads=$BIU_SEARCH_MAX_THREADS"
-echo "biu.search.loadCacheFirst=$BIU_SEARCH_LOAD_CACHE_FIRST"
-echo "biu.search.maxCacheSizeMiB=$BIU_SEARCH_MAX_CACHE_SIZE_MIB"
+echo "biu.search.maxThreads=$BIU_SEARCH_MAXTHREADS"
+echo "biu.search.loadCacheFirst=$BIU_SEARCH_LOADCACHEFIRST"
+echo "biu.search.maxCacheSizeMiB=$BIU_SEARCH_MAXCACHESIZEMIB"
 echo "biu.download.mode=$BIU_DOWNLOAD_MODE"
-echo "biu.download.aria2Host=$BIU_DOWNLOAD_ARIA2_HOST"
-echo "biu.download.aria2Secret=$BIU_DOWNLOAD_ARIA2_SECRET"
-echo "biu.download.deterPaths=$BIU_DOWNLOAD_DETER_PATHS"
-echo "biu.download.maxDownloading=$BIU_DOWNLOAD_MAX_DOWNLOADING"
-echo "biu.download.saveURI=$BIU_DOWNLOAD_SAVE_URI"
-echo "biu.download.saveFileName=$BIU_DOWNLOAD_SAVE_FILE_NAME"
-echo "biu.download.autoArchive=$BIU_DOWNLOAD_AUTO_ARCHIVE"
-echo "biu.download.whatsUgoira=$BIU_DOWNLOAD_WHATS_UGOIRA"
-echo "biu.download.imageHost=$BIU_DOWNLOAD_IMAGE_HOST"
-echo "secret.key.apiSauceNAO=$SECRET_KEY_API_SAUCENAO"
+echo "biu.download.aria2Host=$BIU_DOWNLOAD_ARIA2HOST"
+echo "biu.download.aria2Secret=$BIU_DOWNLOAD_ARIA2SECRET"
+echo "biu.download.deterPaths=$BIU_DOWNLOAD_DETERPATHS"
+echo "biu.download.maxDownloading=$BIU_DOWNLOAD_MAXDOWNLOADING"
+echo "biu.download.saveURI=$BIU_DOWNLOAD_SAVEURI"
+echo "biu.download.saveFileName=$BIU_DOWNLOAD_SAVEFILENAME"
+echo "biu.download.autoArchive=$BIU_DOWNLOAD_AUTOARCHIVE"
+echo "biu.download.whatsUgoira=$BIU_DOWNLOAD_WHATSUGOIRA"
+echo "biu.download.imageHost=$BIU_DOWNLOAD_IMAGEHOST"
+echo "secret.key.apiSauceNAO=$SECRET_KEY_APISAUCENAO"
 
 
-# 清空位置参数
-set --
-# 对于每个参数，只有当对应的环境变量有值时才添加
-if [ -n "$PORT" ]; then
-    set -- "$@" "sys.host=$BASE_URI:$PORT"
-fi
-if [ -n "$SYS_API_ROUTE" ]; then
-    set -- "$@" "sys.apiRoute=$SYS_API_ROUTE"
-fi
-if [ -n "$SYS_PROXY" ]; then
-    set -- "$@" "sys.proxy=$SYS_PROXY"
-fi
-if [ -n "$SYS_LANGUAGE" ]; then
-    set -- "$@" "sys.language=$SYS_LANGUAGE"
-fi
-if [ -n "$SYS_THEME" ]; then
-    set -- "$@" "sys.theme=$SYS_THEME"
-fi
-if [ -n "$BIU_SEARCH_MAX_THREADS" ]; then
-    set -- "$@" "biu.search.maxThreads=$BIU_SEARCH_MAX_THREADS"
-fi
-if [ -n "$BIU_SEARCH_LOAD_CACHE_FIRST" ]; then
-    set -- "$@" "biu.search.loadCacheFirst=$BIU_SEARCH_LOAD_CACHE_FIRST"
-fi
-if [ -n "$BIU_SEARCH_MAX_CACHE_SIZE_MIB" ]; then
-    set -- "$@" "biu.search.maxCacheSizeMiB=$BIU_SEARCH_MAX_CACHE_SIZE_MIB"
-fi
-if [ -n "$BIU_DOWNLOAD_MODE" ]; then
-    set -- "$@" "biu.download.mode=$BIU_DOWNLOAD_MODE"
-fi
-if [ -n "$BIU_DOWNLOAD_ARIA2_HOST" ]; then
-    set -- "$@" "biu.download.aria2Host=$BIU_DOWNLOAD_ARIA2_HOST"
-fi
-if [ -n "$BIU_DOWNLOAD_ARIA2_SECRET" ]; then
-    set -- "$@" "biu.download.aria2Secret=$BIU_DOWNLOAD_ARIA2_SECRET"
-fi
-if [ -n "$BIU_DOWNLOAD_DETER_PATHS" ]; then
-    set -- "$@" "biu.download.deterPaths=$BIU_DOWNLOAD_DETER_PATHS"
-fi
-if [ -n "$BIU_DOWNLOAD_MAX_DOWNLOADING" ]; then
-    set -- "$@" "biu.download.maxDownloading=$BIU_DOWNLOAD_MAX_DOWNLOADING"
-fi
-if [ -n "$BIU_DOWNLOAD_SAVE_URI" ]; then
-    set -- "$@" "biu.download.saveURI=$BIU_DOWNLOAD_SAVE_URI"
-fi
-if [ -n "$BIU_DOWNLOAD_SAVE_FILE_NAME" ]; then
-    set -- "$@" "biu.download.saveFileName=$BIU_DOWNLOAD_SAVE_FILE_NAME"
-fi
-if [ -n "$BIU_DOWNLOAD_AUTO_ARCHIVE" ]; then
-    set -- "$@" "biu.download.autoArchive=$BIU_DOWNLOAD_AUTO_ARCHIVE"
-fi
-if [ -n "$BIU_DOWNLOAD_WHATS_UGOIRA" ]; then
-    set -- "$@" "biu.download.whatsUgoira=$BIU_DOWNLOAD_WHATS_UGOIRA"
-fi
-if [ -n "$BIU_DOWNLOAD_IMAGE_HOST" ]; then
-    set -- "$@" "biu.download.imageHost=$BIU_DOWNLOAD_IMAGE_HOST"
-fi
-if [ -n "$SECRET_KEY_API_SAUCENAO" ]; then
-    set -- "$@" "secret.key.apiSauceNAO=$SECRET_KEY_API_SAUCENAO"
-fi
 echo "Starting PixivBiu..."
-echo "Final command arguments:$@"
-exec sudo -u "#$PUID" -g "#$PGID" "$@" /Pixiv/main
+exec sudo -u "#$PUID" -g "#$PGID" /Pixiv/main
